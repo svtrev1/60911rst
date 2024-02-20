49ec0b54c62c38c99f88aebd6476034830f0575b
svtrev <revtsova_st@edu.surgu.ru>
2024-02-20 18:51:28 +0500

    fix problems

--- database/migrations/2024_02_20_133454_create_sessions_table.php
+++ database/migrations/2024_02_20_133454_create_sessions_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->dateTime('start_datetime');
             $table->dateTime('end_datetime');
             $table->timestamps();
-
         });
     }
 
